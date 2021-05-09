047d17f2a5c401031ce5fd41519e607fe8135cbf
Louisa Molin <louisamolin@gmail.com>
2021-05-09 15:41:26 -0400

    completed (?) entity model classes, added hibernate.cfg.xml

--- src/main/java/jpa/entitymodels/Course.java
+++ src/main/java/jpa/entitymodels/Course.java
@@ -3,13 +3,24 @@ package jpa.entitymodels;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "Course")
 public class Course {
+    @Column (name = "id")
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private int cId;
+    @Column (name = "name")
     private String cName;
+    @Column (name = "instructor")
     private String cInstructorName;
 
+    public Course() {
+        cName = "null";
+        cInstructorName = "null";
+    }
 
+    public Course(String cName, String cInstructorName) {
+        this.cName = cName;
+        this.cInstructorName = cInstructorName;
+    }
 
     //getters and setters
 
--- src/main/java/jpa/entitymodels/Student.java
+++ src/main/java/jpa/entitymodels/Student.java
@@ -1,4 +1,57 @@
 package jpa.entitymodels;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Student {
+    @Id
+    @Column (name = "email")
+    private String sEmail;
+    @Column (name = "name")
+    private String sName;
+    @Column (name = "password")
+    private String sPass;
+    private List<Course> sCourses;
+
+    public Student() {
+        sEmail = "null";
+        sName = "null";
+        sPass = "null";
+
+    }
+
+    //getters and setters
+
+    public String getsEmail() {
+        return sEmail;
+    }
+
+    public void setsEmail(String sEmail) {
+        this.sEmail = sEmail;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public String getsPass() {
+        return sPass;
+    }
+
+    public void setsPass(String sPass) {
+        this.sPass = sPass;
+    }
+
+    public List<Course> getsCourses() {
+        return sCourses;
+    }
+
+    public void setsCourses(List<Course> sCourses) {
+        this.sCourses = sCourses;
+    }
 }
